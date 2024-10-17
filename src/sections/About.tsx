a5e5793e6c44074319e14f27f6b5ff89056db543
Marcin <marsobpro@gmail.com>
2024-10-17 20:18:02 +0200

    Updated map and memoji

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 import SectionHeader from "@/components/SectionHeader";
-import StarIcon from "@/assets/icons/star.svg";
 import Card from "@/components/Card";
 import bookImage from "@/assets/images/book-cover.png";
 import Image from "next/image";
@@ -10,13 +11,10 @@ import CssIcon from "@/assets/icons/css3.svg";
 import ReactIcon from "@/assets/icons/react.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import GithubIcon from "@/assets/icons/github.svg";
-import TechIcon from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.png";
 import smileMemoji from "@/assets/images/memoji-smile.png";
 import CardHeader from "@/components/CardHeader";
 import ToolboxItems from "@/components/ToolboxItems";
-import { motion } from "framer-motion";
-import { useRef } from "react";
 
 const toolboxItems = [
   {
@@ -137,7 +135,7 @@ export const AboutSection = () => {
 
                 <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
                 <Image
-                  className="size-20"
+                  className="size-20 object-cover"
                   src={smileMemoji}
                   alt="smiling memoji"
                 />
