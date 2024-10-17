80bf58c467c51bc3cc3ea192e4229eef475450a8
Marcin <marsobpro@gmail.com>
2024-10-17 11:07:18 +0200

    Added content for section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,23 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
+          <h2 className="font-serif text-2xl">
+            Let's create something amazing together!
+          </h2>
+          <p className="text-sm mt-2">
+            Ready to bring your next project to life? Let's connect and discuss
+            how I can help you achieve your goals.
+          </p>
+          <button>
+            <span>Contact Me</span>
+            <ArrowUpRightIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
