371f89e22e0f9f15407d7bc7834b4e3c163db3a3
Marcin <marsobpro@gmail.com>
2024-10-17 11:15:23 +0200

    Added design for mobile

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,10 +1,15 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 export const ContactSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 pt-12">
       <div className="container">
-        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 z-1 opacity-5 -z-10"
+            style={{ backgroundImage: `url(${grainImage.src})` }}
+          />
           <h2 className="font-serif text-2xl">
             Let's create something amazing together!
           </h2>
@@ -12,9 +17,9 @@ export const ContactSection = () => {
             Ready to bring your next project to life? Let's connect and discuss
             how I can help you achieve your goals.
           </p>
-          <button>
-            <span>Contact Me</span>
-            <ArrowUpRightIcon />
+          <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-8 mt-8">
+            <span className="font-semibold">Contact Me</span>
+            <ArrowUpRightIcon className="size-4" />
           </button>
         </div>
       </div>
