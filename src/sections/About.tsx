458875094daa0e7923da2795debaa06c6e0b2178
Marcin <marsobpro@gmail.com>
2024-10-17 19:21:46 +0200

    Added animation for map card

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -119,14 +119,17 @@ export const AboutSection = () => {
               </div>
             </Card>
 
-            {/* Location */}
+            {/* Map */}
             <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
               <Image
                 className="h-full w-full object-cover object-left-top"
                 src={mapImage}
                 alt="map"
               />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:rounded-full after:-outline-offset-2 after:outline-gray-950/30">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:rounded-full after:-outline-offset-2 after:outline-gray-950/30">
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
                 <Image
                   className="size-20"
                   src={smileMemoji}
