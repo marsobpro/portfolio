851ee43bd3e2d38d84ce7fd4c9cacc4b3792bf4e
Marcin <marsobpro@gmail.com>
2024-10-17 18:54:35 +0200

    Added tape animation by adding move-left animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
       },
       animation: {
         "ping-large": "ping-large 1s ease-in-out infinite",
+        "move-left": "move-left 25s linear infinite",
       },
       keyframes: {
         "ping-large": {
@@ -34,6 +35,10 @@ const config: Config = {
             transform: "scale(3)",
             opacity: "0",
           },
+        },
+        "move-left": {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" },
         },
       },
     },
